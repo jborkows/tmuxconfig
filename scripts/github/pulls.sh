98daf0a10b232e94bd83ba362bafe8ba4d3de0fd
Jacek Borkowski (borkowski.jb) <jacek.borkowski@lingarogroup.com>
2024-05-06 12:17:21 +0200

    change pulls

--- scripts/github/pulls.sh
+++ scripts/github/pulls.sh
@@ -5,6 +5,6 @@ export FZF_DEFAULT_OPTS="--preview 'batcat -n --color=always {}' --bind 'f1:togg
 echo "${repo}" | xargs -I {} gh pr list --json number,state,title,author,id,labels,isDraft,comments,createdAt,updatedAt,url,mergeable --repo {} --limit 100 |
 	jq -r '.[] | "\(.updatedAt) -> \(.title) \(.author) \(.mergeable)\t\(.| @base64)"' | fzf --delimiter='\t' \
         --header='enter to open' \
-	--preview='echo {2} | base64 --decode | jq -r '\''"Name \(.title) \(.id) \(.isDraft) \nCreated At \(.createdAt) Updated \(.updatedAt)\nStatus \(.status)\n\(.url)\n Comments \(.comments)"'\'''  \
-        | sed 's/\s+/ /' | sed 's/\t/ /' | cut -f4 -d ' ' |base64 --decode| jq -r '.url' |   xargs -I {} firefox {} ;
+	--preview='echo {2} | base64 --decode | jq -r '\''"Name \(.title) \(.number) Draft \(.isDraft) \nCreated At \(.createdAt) Updated \(.updatedAt)\nAuthor \(.author.login)\n\(.url)\n Comments \n  \(.comments[].author.login) \(.comments[].body)"'\'''  \
+        | cut -f2 -d$'\t'  |  sed 's/\s+//' | base64 --decode| jq -r '.url' |   xargs -I {} firefox {} ;
 
