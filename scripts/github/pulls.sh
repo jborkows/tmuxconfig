c977a3236b2a8afbe4c3100342e1356193044f36
Jacek Borkowski (borkowski.jb) <jacek.borkowski@lingarogroup.com>
2024-05-06 19:07:17 +0200

    if null do not display

--- scripts/github/pulls.sh
+++ scripts/github/pulls.sh
@@ -5,6 +5,12 @@ export FZF_DEFAULT_OPTS="--preview 'batcat -n --color=always {}' --bind 'f1:togg
 echo "${repo}" | xargs -I {} gh pr list --json number,state,title,author,id,labels,isDraft,comments,createdAt,updatedAt,url,mergeable --repo {} --limit 100 |
 	jq -r '.[] | "\(.updatedAt) -> \(.title) \(.author) \(.mergeable)\t\(.| @base64)"' | fzf --delimiter='\t' \
         --header='enter to open' \
-	--preview='echo {2} | base64 --decode | jq -r '\''"Name \(.title) \(.number) Draft \(.isDraft) \nCreated At \(.createdAt) Updated \(.updatedAt)\nAuthor \(.author.login)\n\(.url)\n Comments \n  \(.comments[].author.login) \(.comments[].body)"'\'''  \
+	--preview='echo {2} | base64 --decode | jq -r '\''"
+Name \(.title) \(.number) Draft \(.isDraft)
+Created At \(.createdAt) Updated \(.updatedAt)
+Author \(.author.login)
+\(.url)
+Comments 
+  \(if .comments | length > 0 then (.comments[] | "\(.author.login) \(.body)") else "No comments" end)"   '\'''  \
         | cut -f2 -d$'\t'  |  sed 's/\s+//' | base64 --decode| jq -r '.url' |   xargs -I {} firefox {} ;
 
