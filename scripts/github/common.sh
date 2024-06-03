43a66017e83a8f8ee5083a606b40cd7f496c82b2
jborkows <jacek.borkowskiit@gmail.com>
2024-06-04 00:20:25 +0200

    add removal of first / if it is first

--- scripts/github/common.sh
+++ scripts/github/common.sh
@@ -1,1 +1,1 @@
-export repo=$(git remote -v |grep origin| sed "s/:/ /" | sed "s/\s/ /" |cut -d" " -f3 |sed "s/\s*//"|sed "s/\.git//" | uniq )
+export repo=$(git remote -v |grep origin| sed "s/:/ /" | sed "s/\s/ /" |cut -d" " -f3 |sed "s/\s*//"|sed "s/\.git//" | uniq | sed 's|^/||')
